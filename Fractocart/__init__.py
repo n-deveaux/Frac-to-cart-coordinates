3095e5cee9456b1923134e86556c3e951fcf159a
Noah Deveaux <noah.deveaux@unamur.be>
2024-12-10 10:10:00 +0100

    Fix issue when importing the functions

--- Fractocart/__init__.py
+++ Fractocart/__init__.py
@@ -7,3 +7,5 @@ __author__ = "Noah Deveaux"
 __maintainer__ = "Noah Deveaux"
 __email__ = "noah.deveaux@unamur.be"
 __status__ = "Development"
+
+from .main import build_transformation_matrix, convert_to_cartesian_coordinates, convert_to_fractional_coordinates
